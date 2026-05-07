6042929bbcaa468881483f6c985fc841905065ea
Arda Onur <ardaonur15@gmail.com>
2026-05-07 12:40:37 +0300

    BuilderPattern example

---
+++ src/main/java/com/example/designpatterns/BuilderPattern/User.java
@@ -0,0 +1,48 @@
+package com.example.designpatterns.BuilderPattern;
+
+public class User {
+
+    private final String name;
+    private final String email;
+    private final int age;
+
+    private User(Builder builder) {
+        this.name = builder.name;
+        this.email = builder.email;
+        this.age = builder.age;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private String email;
+        private int age;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
